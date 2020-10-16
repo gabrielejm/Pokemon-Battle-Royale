bed54962de5c1b794e8370ae5655781d07d461fe
Cmac <mckoyc@gmail.com>
2020-10-16 18:52:30 -0400

    created skeleton of the fight preveiw

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function compTeamCreator() {
 }
 
 
-compTeamCreator()
+// compTeamCreator()
 
 
 //task 5:
@@ -105,6 +105,79 @@ compTeamCreator()
 //when the user clicks one of their pokemon choices, a modal appears as the "battlefield" with the user choice pokemon and ai choice pokemon
 //the modal should have a "fight" button
 
+function battlePreveiw () {
+    var battlefield = $('<div>')
+    var userPokemon = $('<div>')
+    var computerPokemon = $('<div>')
+    var versus = $('<div>')
+
+    userPokemon.css({
+        "background": "red",
+        "height" : "250px",
+        "width": "250px",
+        "float": "left",
+        "text-align": "center",
+    })
+    var userName = $('<h2>')
+    var userImg = $('<img>')
+    userName.text("Bulbasaur")
+    userImg.attr("src","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png")
+    userImg.css({
+      "height": "150px",
+      "width" : "150px",
+    })
+
+    userPokemon.append(userName)
+    userPokemon.append(userImg)
+
+    computerPokemon.css({
+        "background": "white",
+        "border": "1px black",
+        "height" : "250px",
+        "width": "250px",
+        "float": "right",
+        "text-align": "center"
+    })
+    
+    var aiName = $('<h2>')
+    var aiImg = $('<img>')
+
+    aiName.text("Bulbasaur")
+    aiImg.attr("src","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png")
+    aiImg.css({
+      "height": "150px",
+      "width" : "150px",
+    })
+    
+    computerPokemon.append(aiName)
+    computerPokemon.append(aiImg)
+
+    versus.css({
+        "background" : "black",
+        "color" : "white",
+        "width" : "50px",
+        "height": "150px",
+        "float" : "center",
+    })
+    versus.text("VS")
+
+    // var fieldImg = "https://i.redd.it/o8a7u5vl6hb41.png"
+    battlefield.css({
+        // "background-image": "url(" + fieldImg + ")",
+        "background" : "grey",
+        "height": "200px",
+        "width": "600px",
+        // "z-index" : "1",
+    })
+
+    battlefield.append(userPokemon)
+    battlefield.append(versus)
+    battlefield.append(computerPokemon)
+    
+    $('body').append(battlefield)
+}
+
+battlePreveiw()
 //task 6:
 //when fight button is clicked, a winner determined by:
   //the pokemon with the higher speed stat attacks first
